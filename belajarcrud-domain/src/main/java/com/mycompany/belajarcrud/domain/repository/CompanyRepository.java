58371bbfdbbb1b5533d8412cee1f2ec923eb2219
Amelia Hermayanti <amelia.hrmynt@gmail.com>
2019-04-16 18:12:42 +0700

    master company

--- belajarcrud-domain/src/main/java/com/mycompany/belajarcrud/domain/repository/CompanyRepository.java
+++ belajarcrud-domain/src/main/java/com/mycompany/belajarcrud/domain/repository/CompanyRepository.java
@@ -10,7 +10,7 @@ import javax.transaction.Transactional;
 import org.springframework.stereotype.Repository;
 
 /**
- * 
+ *
  * @author eksad
  */
 @Repository
--- belajarcrud-svc/src/main/java/com/mycompany/belajarcrud/svc/CompanyRESTController.java
+++ belajarcrud-svc/src/main/java/com/mycompany/belajarcrud/svc/CompanyRESTController.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package com.mycompany.belajarcrud.svc;
- 
+
 import com.mycompany.belajarcrud.domain.Company;
 import com.mycompany.belajarcrud.domain.assembler.CompanyAssembler;
 import com.mycompany.belajarcrud.domain.repository.CompanyRepository;
